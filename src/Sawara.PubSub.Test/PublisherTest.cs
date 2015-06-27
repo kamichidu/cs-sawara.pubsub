c6c03820b8e7a565035cc206888344c6063c18e0
kamichidu <c.kamunagi@gmail.com>
2015-06-27 11:29:45 +0900

    Add a test

---
+++ src/Sawara.PubSub.Test/PublisherTest.cs
@@ -0,0 +1,34 @@
+﻿namespace Sawara.PubSub.Test
+{
+    using Sawara.PubSub;
+    using Xunit;
+
+    public class PublisherTest
+    {
+        [Fact]
+        public void Publisher_publishes_to_any_subscribers()
+        {
+            var publisher = new Publisher<string>();
+
+            publisher.Publish("test");
+
+            int ncalls = 0;
+            Subscriber<string> subscriber = (string val) =>
+            {
+                Assert.Equal("publish", val);
+                ++ncalls;
+            };
+            publisher.Subscribe(subscriber);
+
+            Assert.Equal(0, ncalls);
+            publisher.Publish("publish");
+            Assert.Equal(1, ncalls);
+
+            publisher.Unsubscribe(subscriber);
+
+            Assert.Equal(1, ncalls);
+            publisher.Publish("no one subscribes");
+            Assert.Equal(1, ncalls);
+        }
+    }
+}
